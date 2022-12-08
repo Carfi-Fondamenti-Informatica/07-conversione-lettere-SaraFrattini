83bcc1fe5736417dcd8a784c3bfe69afbe259269
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-08 16:19:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    char n=0;
+    cin>>n;
+    if (Giangiorgio(n)==true)
+        cout<< n <<endl;
+    else
+        cout<<"errore"<<endl;
+    return 0;
 }
